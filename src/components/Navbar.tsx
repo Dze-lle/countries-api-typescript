ec4c38d36f4cb64cf64d6442970884abf6d9ab04
Elmer Mendieta <emendieta60236@ufide.ac.cr>
2022-10-09 12:34:35 -0600

    styling navbar and adding medias

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,13 @@ const Navbar = () => {
   const { darkMode, dispatch } = useTheme();
 
   return (
-    <nav>
-      <div></div>
-      <div>
-        <button
-          onClick={() => dispatch({ type: "TOGGLE_MODE", payload: !darkMode })}
-        >
-          dark mode
-        </button>
-      </div>
+    <nav className="nav">
+      <h1>where in the world?</h1>
+      <button
+        onClick={() => dispatch({ type: "TOGGLE_MODE", payload: !darkMode })}
+      >
+        dark mode
+      </button>
     </nav>
   );
 };
