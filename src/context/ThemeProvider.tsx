2bde562a63ddf3f7df1dd928d75230d00bca563c
Elmer Mendieta <emendieta60236@ufide.ac.cr>
2022-10-10 18:21:26 -0600

    localStorage Added

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -3,8 +3,8 @@ import { IGlobalTheme } from "../interfaces";
 import { ThemeContext } from "./ThemeContext";
 import { ThemeReducer } from "./ThemeReducer";
 
-const { darkMode } = localStorage.getItem("themeMode")
-  ? JSON.parse(localStorage.getItem("themeMode")!)
+const { darkMode } = localStorage.getItem("ThemeState")
+  ? JSON.parse(localStorage.getItem("ThemeState")!)
   : false;
 
 const INITIAL_STATE: IGlobalTheme = {
@@ -19,7 +19,7 @@ export const ThemeProvider = ({ children }: props) => {
   const [globalTheme, dispatch] = useReducer(ThemeReducer, INITIAL_STATE);
 
   useEffect(() => {
-    localStorage.setItem("themeMode", JSON.stringify(globalTheme));
+    localStorage.setItem("ThemeState", JSON.stringify(globalTheme));
   }, [globalTheme.darkMode]);
 
   return (
