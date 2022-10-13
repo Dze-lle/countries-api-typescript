4da7bec4a66aa13b86952a02c0be53d3ce9083a7
Elmer Mendieta <emendieta60236@ufide.ac.cr>
2022-10-12 22:35:55 -0600

    styling container items

--- src/components/container/ListCountries.tsx
+++ src/components/container/ListCountries.tsx
@@ -1,5 +1,17 @@
 const ListCountries = () => {
-  return <div>ListCountries is working</div>;
+  return (
+    <article>
+      <div className="flag">
+        <img src="" alt="" />
+      </div>
+      <div className="details">
+        <p></p>
+        <strong></strong>
+        <strong></strong>
+        <strong></strong>
+      </div>
+    </article>
+  );
 };
 
 export default ListCountries;
--- src/components/container/SearchCountries.tsx
+++ src/components/container/SearchCountries.tsx
@@ -1,8 +1,16 @@
+import { FaSearch } from "react-icons/fa";
+
 const SearchCountries = () => {
   return (
-    <div>
-      <input type="search" name="" id="" />
+    <div className="form-group">
+      <div className="search">
+        <FaSearch className="icon" />
+        <input type="search" name="" id="" placeholder="Search for a country" />
+      </div>
       <select name="countries" id="countries">
+        <option className="option" value="0">
+          filter by region
+        </option>
         <option value="africa">africa</option>
         <option value="america">america</option>
         <option value="asia">asia</option>
