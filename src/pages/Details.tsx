4fb81a50107f8cab2da476785ed7f9a455ab256e
Elmer Mendieta <emendieta60236@ufide.ac.cr>
2022-11-03 18:21:47 -0600

    details.css almost done

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -18,29 +18,58 @@ const Details = () => {
 
   return (
     <section className="details">
-      <Link to="/countries-api-typescript" className="details__button">
-        <FaLongArrowAltLeft />
-        <span>back</span>
-      </Link>
+      <div className="details__button">
+        <Link to="/countries-api-typescript" className="btn">
+          <FaLongArrowAltLeft className="arrow-icon" />
+          <span>back</span>
+        </Link>
+      </div>
 
       <div className="details__container">
         <img src={country?.flags.png} alt={country?.name} />
         <div className="country">
+          <h4>{country?.name}</h4>
           <div className="country__left">
-            <h4>{country?.name}</h4>
-            <strong>native name:</strong> {country?.nativeName}
-            <strong>region:</strong> {country?.region}
-            <strong>sub region:</strong> {country?.subregion}
-            <strong>capital:</strong> {country?.capital}
+            <span>
+              <strong>native name:</strong> {country?.nativeName}
+            </span>
+            <span>
+              <strong>population:</strong>{" "}
+              {country?.population.toLocaleString("en-US")}
+            </span>
+            <span>
+              <strong>region:</strong> {country?.region}
+            </span>
+            <span>
+              <strong>sub region:</strong> {country?.subregion}
+            </span>
+            <span>
+              <strong>capital:</strong> {country?.capital}
+            </span>
           </div>
           <div className="country__right">
-            <strong>top level domain:</strong> {country?.topLevelDomain}
-            <strong>currencies:</strong>{" "}
-            {country?.currencies.map((a) => a.name)}
-            <strong>languages:</strong>{" "}
-            {country?.languages.map((a) => a.name).join(", ")}
+            <span>
+              <strong>top level domain:</strong> {country?.topLevelDomain}
+            </span>
+            <span>
+              <strong>currencies:</strong>{" "}
+              {country?.currencies.map((a) => a.name)}
+            </span>
+            <span>
+              <strong>languages:</strong>{" "}
+              {country?.languages.map((a) => a.name).join(", ")}
+            </span>
           </div>
-          <strong className="country__border">border countries:</strong>
+          {country?.borders === undefined ? (
+            <></>
+          ) : (
+            <div className="country__border">
+              <strong>border countries:</strong>
+              {country.borders.map((code, index) => (
+                <span key={index}>{code}</span>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </section>
